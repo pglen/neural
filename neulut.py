9525f5d4289dbdcc63d8bfece35cd2ca208593c2
Peter Glen <peterglen@gmail.com>
2024-07-16 06:54:28 -0400

    import_old

--- neulut.py
+++ neulut.py
@@ -8,18 +8,21 @@ import random, math, sys
 from neuutil import *
 from pgutil import *
 
-verbose = 0
-
+VERBOSE = 0
+PGDEBUG = 1
+QUADRATIC = 0
 # Help identify a neuron by serial number
 
 gl_serial = 0
 
 # ------------------------------------------------------------------------
-# The basic building block
-# The training material is pushed to an array;
-# The lookup is executed finding the closest match
 
 class NeuLut():
+
+    '''     The basic building block of neural lookup table.
+            The training material is pushed to an array;
+            The lookup is executed finding the closest match.
+    '''
 
     def __init__(self, inputs, outputs):
 
@@ -28,42 +31,45 @@ class NeuLut():
         # These are helpers
         self.serial = gl_serial; gl_serial += 1;
 
-        if verbose:
-            print("neulut init ",  "inuts %.03f " % inputs) #, end=' ')
+        if VERBOSE:
+            print("NeuLut init ",  "inuts %.03f " % inputs) #, end=' ')
 
-        self.inputs  = []; self.outputs = []
-        self.strength = 0
+        self.inputs  = []; self.outputs = []; self.trarr = []
+
+        # Alloc, provide defaults
         for aa in range(inputs):
-            #self.inputs.append(neurand())
             self.inputs.append(0.)
 
         for aa in range(outputs):
             self.outputs.append(0.)
 
-        self.trarr = []
+        self.trarr = []; self.resarr = []
 
     def inlen(self):
         return len(self.inputs)
 
-    # --------------------------------------------------------------------
-    # Compare arrays, return largest mismatch value
-    # Obey step value
+    def outlen(self):
+        return len(self.outputs)
 
-    def cmp(self, ins, val, step, stride):
+    def _cmp(self, ins, ref, step = 1, stride = 1):
 
+        '''  Compare arrays, return sum of mismatch value.
+             Obey step value. The flag QUADRATIC will use
+             the squre function.
+        '''
         #print("cmp", ins, val, "step", step, "strde", stride)
-
         ddd = []; res2 = 0.
         prog = 0; prog2 = 0
         try:
             while True:
-
                 if prog2 >= len(ins):
                     break
-
-                for aa in range(step):
-                    #print("ooo", aa, org[aa], val[aa], end=" ")
-                    ddd.append(sqr(ins[prog2 + aa] - val[prog + aa]))
+                if prog >= len(ref):
+                    break
+                #diff = sqr(ins[prog2] - ref[prog])
+                diff = ins[prog2] - ref[prog]
+                #print("diff", diff)
+                ddd.append(diff)
                 prog  +=  step
                 prog2 +=  stride
 
@@ -78,7 +84,7 @@ class NeuLut():
         #print("ddd", end = " "); parr(ddd)
         for bb in ddd:
             #res2 = math.sqrt(sqr(bb) + sqr(res2))
-            res2 += bb
+            res2 += abs(bb)
 
         if len(ddd):
             return res2 / len(ddd)
@@ -86,98 +92,124 @@ class NeuLut():
             return 0
 
     # --------------------------------------------------------------------
-    # Fire one neuron. Find smallest diff.
-
     def fire(self, ins, stride):
+
+        ''' Fire one neuron. Sum all diffs, div by count. '''
+
         #print("fire", ins[:12])
-        old = 0xffff ; ooo = []; sss = 0
-        for aa in self.trarr:
-            #print("firex",  aa[0][:12], end = "\n")
-            ss = self.cmp(ins, aa[0], aa[2], stride)
+        old = 0xffff; idx = -1; sss = 0
+        #print("  ins", ins)
+        for aa in range(len(self.trarr)):
+            ref = self.trarr[aa]
+            ss = self._cmp(ins, ref, stride)
+            #print("   train:", ref, "diff:", ss)
             if old > ss:
                 old = ss
-                ooo = aa[1]
-                sss = ss
-
-        self.outputs = ooo
-        self.strength = sss
-        return ooo
-
-    def fire_one(self, offs, ins, stride):
-
-        #print("fire_one", ins[:12])
-        ss = self.cmp(ins, self.trarr[offs][0], self.trarr[offs][2], stride)
-        self.outputs = self.trarr[offs][1]
-        self.strength = ss
-        return self.trarr[offs][1]
+                idx = aa
+        self.outputs = self.resarr[idx]
+        return self.outputs
 
     def __str__(self):
-        return "in: " + str(self.inputs)[:20]  + " ... out: " + \
-                    str(self.outputs)[:20] + " ..."
+        return "ins: " + str(self.inputs)[:20]  + " outs: " + \
+                    str(self.outputs)[:20]
 
     def dump(self):
-        for aa in self.trarr:
-           print(aa)
+        for aa in enumerate(self.trarr):
+           print("tr:", aa[1], "\t  rs:", self.resarr[aa[0]])
 
     def train(self, ins, outs, step = 1):
         #print(ins, outs)
-        self.trarr.append((ins, outs, step))
+        self.trarr.append(ins)
+        self.resarr.append(outs)
 
     def showtrain(self):
         for aa in self.trarr:
             print(aa)
 
+def is_ok(val, ref):
+    if val == ref:
+        print("\033[32;1mOK\033[0m", end = " ")
+    else:
+        print("\033[31;1mERR\033[0m", end = " ")
 
-VAL  = 0.5
-VAL2 = 0.6
+# Decorator for speed measure
+def measure(func):
+    def run(*args, **kwargs):
+        ttt = time.time()
+        ret = func(*args, **kwargs)
+        print("Exe: %.3f us" % ((time.time() - ttt) * 1000000))
+        return ret
+    return run
 
-# imitate and gate
+#@measure
+def testx(kind, in_arr, out_arr, tin_arr, tout_arr):
 
-in_arr =  ( (0, 0), (VAL, 0), (0, VAL), (VAL, VAL) )
-ou_arr =  ((0,0,), (0,0,), (0,0,), (VAL,0,))
-
-tin_arr =  ( (0, 0), (VAL2, 0), (0, VAL2), (VAL2, VAL2) )
-tou_arr =  ((0,0,), (0,0,), (0,0,), (VAL,0,))
-
-# imitate or gate
-
-in_oarr =  ( (0, 0), (VAL, 0), (0, VAL), (VAL, VAL) )
-ou_oarr =  (0, VAL, VAL, VAL,)
-
-tin_oarr =  ( (0, 0), (VAL2, 0), (0, VAL2), (VAL2, VAL2) )
-tou_oarr =  (0, VAL, VAL, VAL,)
-
-
-if __name__ == '__main__':
-    nn = neulut(2, 2)
-    print("neulut AND", nn)
+    nn = NeuLut(2, 1)
+    #print("NeuLut %s:" % kind, nn)
 
     for aa in range(len(in_arr)):
-        nn.train(in_arr[aa], ou_arr[aa])
+        nn.train(in_arr[aa], out_arr[aa])
 
-    #print(nn)
     #nn.dump()
+    #sys.exit(0)
 
     for aa in range(len(tin_arr)):
-        nn.fire(tin_arr[aa])
-        print(tin_arr[aa], tou_arr[aa], "out",  nn.outputs,
-            "%.3f" % nn.strength)
+        nn.fire(tin_arr[aa], 1)
+        print(kind, "in:", tin_arr[aa], "out:",
+                nn.outputs, "expect:", tout_arr[aa], end = " ")
+        is_ok(nn.outputs,  tout_arr[aa])
+        print()
+
+''' The generalzation comes from the lattitude of the compare. For
+instance, using less than 0.5 for VAL2, the logic interprets it as zero, using
+greater than 0.5, the interpreetation is one.
+'''
+
+VAL  = 1.
+VAL2 = 0.501
+OUT = 1.
+
+if __name__ == '__main__':
+
+    # imitate the AND gate
+
+    in_arr =  ( (0, 0), (VAL, 0), (0, VAL), (VAL, VAL) )
+    out_arr =  (0, 0, 0, OUT)
+    tin_arr =  ( (0, 0), (VAL2, 0), (0, VAL2), (VAL2, VAL2) )
+    tout_arr =  (0, 0, 0, OUT)
+
+    #ttt = time.time()
+    testx("AND ", in_arr, out_arr, tin_arr, tout_arr)
+    #print("Exe: %.3f us" % ((time.time() - ttt) * 1000000))
 
     # -----------------------------------------------------------
+    # imitate the OR gate
 
-    nn = neulut(2, 1)
-    print("\nneulut OR", nn)
+    in_oarr =  ( (0, 0), (VAL, 0), (0, VAL), (VAL, VAL) )
+    out_oarr =  (0, OUT, OUT, OUT,)
+    tin_oarr =  ( (0, 0), (VAL2, 0), (0, VAL2), (VAL2, VAL2) )
+    tout_oarr =  (0, VAL, VAL, VAL,)
 
-    for aa in range(len(in_arr)):
-        nn.train(in_oarr[aa], ou_oarr[aa])
+    testx("OR  ", in_oarr, out_oarr, tin_oarr, tout_oarr)
 
-    #print(nn)
-    #nn.dump()
+    # -----------------------------------------------------------
+    # imitate the XOR gate
 
-    for aa in range(len(tin_oarr)):
-        nn.fire(tin_oarr[aa])
-        print(tin_oarr[aa], tou_oarr[aa], "out",  nn.outputs,
-            "%.3f" % nn.strength)
+    in_xarr =  ( (0, 0), (VAL, 0), (0, VAL), (VAL, VAL) )
+    out_xarr =  (0, OUT, OUT, 0, )
+    tin_xarr =  ( (0, 0), (VAL2, 0), (0, VAL2), (VAL2, VAL2) )
+    tout_xarr =  (0, OUT, OUT, 0, )
 
+    testx("XOR ", in_xarr, out_xarr, tin_xarr, tout_xarr)
+
+    # -----------------------------------------------------------
+    # imitate the NAND gate
+
+    in_xarr =  ( (0, 0), (VAL, 0), (0, VAL), (VAL, VAL) )
+    out_xarr =  (0, OUT, OUT, OUT, )
+    tin_xarr =  ( (0, 0), (VAL2, 0), (0, VAL2), (VAL2, VAL2) )
+    tout_xarr =  (0, OUT, OUT, OUT, )
+
+    testx("NAND", in_xarr, out_xarr, tin_xarr, tout_xarr)
 
 # EOF
